003fc1ad689c3101307ae9a5b500b5b02ed7ddd1
doakcovington <doakcovington@protonmail.com>
2020-03-18 21:30:30 -0700

    Added Executable for scraper.rb

--- lib/go_nogo/scraper.rb
+++ lib/go_nogo/scraper.rb
@@ -1,7 +1,14 @@
 class GoNoGo::Scraper
     
-    def self.scrape_mountain_passes
+    #pass url for blewett https://www.wsdot.com/traffic/passes/blewett/default.aspx
+    def self.scrape_mountain_passes(pass_url)
      #doc.css("div#PassPageBoxPanel.content")
+     #temp = doc.css("span#PassInfoTemperature")
+     #elevation = doc.css("span#PassInfoElevationF")
+     #restrictions one = doc.css("span#PassInfoRestrictionsOne")
+     #restrictions two = doc.css("span#PassInfoRestrictionsTwo")
+     #conditions = doc.css("span#PassInfoConditions")
+     #weather = doc.css("span#PassInfoWeather")
     end
 
 end
